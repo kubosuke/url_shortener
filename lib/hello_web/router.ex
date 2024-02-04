48e2c4fb699e0e94ad550db6ae39e73ad0b49013
Keisuke Kubota <Kk.bb1130@outlook.jp>
2024-02-04 16:24:09 +0900

    Add auth under /url

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -1,6 +1,8 @@
 defmodule HelloWeb.Router do
   use HelloWeb, :router
 
+  import HelloWeb.AdminAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,11 @@ defmodule HelloWeb.Router do
     plug :put_root_layout, html: {HelloWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_admin
+  end
+
+  pipeline :admin do
+    plug :require_authenticated_admin
   end
 
   pipeline :api do
@@ -18,11 +25,13 @@ defmodule HelloWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
-    resources "/url", URLController
-    get "/goto/:hash", RedirectController, :index
 
-    # get "/hello", HelloController, :index
-    # get "/hello/:messenger", HelloController, :show
+    scope "/url" do
+      pipe_through :admin
+      resources "/", URLController
+    end
+
+    get "/goto/:hash", RedirectController, :index
   end
 
   # Other scopes may use custom stacks.
@@ -44,6 +53,44 @@ defmodule HelloWeb.Router do
 
       live_dashboard "/dashboard", metrics: HelloWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", HelloWeb do
+    pipe_through [:browser, :redirect_if_admin_is_authenticated]
+
+    live_session :redirect_if_admin_is_authenticated,
+      on_mount: [{HelloWeb.AdminAuth, :redirect_if_admin_is_authenticated}] do
+      live "/admins/register", AdminRegistrationLive, :new
+      live "/admins/log_in", AdminLoginLive, :new
+      live "/admins/reset_password", AdminForgotPasswordLive, :new
+      live "/admins/reset_password/:token", AdminResetPasswordLive, :edit
+    end
+
+    post "/admins/log_in", AdminSessionController, :create
+  end
+
+  scope "/", HelloWeb do
+    pipe_through [:browser, :require_authenticated_admin]
+
+    live_session :require_authenticated_admin,
+      on_mount: [{HelloWeb.AdminAuth, :ensure_authenticated}] do
+      live "/admins/settings", AdminSettingsLive, :edit
+      live "/admins/settings/confirm_email/:token", AdminSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", HelloWeb do
+    pipe_through [:browser]
+
+    delete "/admins/log_out", AdminSessionController, :delete
+
+    live_session :current_admin,
+      on_mount: [{HelloWeb.AdminAuth, :mount_current_admin}] do
+      live "/admins/confirm/:token", AdminConfirmationLive, :edit
+      live "/admins/confirm", AdminConfirmationInstructionsLive, :new
     end
   end
 end
